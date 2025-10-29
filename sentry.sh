e091acf72b9b6a08ca1b1954a25b1fdd63876a87
Jon Leibowitz <jleibowitz@dpw.lacounty.gov>
2025-10-28 17:08:07 -0700

    the sentry script just does hello world

---
+++ sentry.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Quick and dirty "Hello World" using Charm's gum
+
+gum style --foreground 212 --border double --align center 'Hello, World!'
+
+# How to run:
+# chmod +x sentry.sh && ./sentry.sh
